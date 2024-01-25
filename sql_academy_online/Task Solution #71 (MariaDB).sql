2836e5294b8af77672533dd27f5dbcf9a1b1f7c7
Mark Saburov <147604070+sudomango@users.noreply.github.com>
2024-01-25 17:42:15 +0300

    Добавлено несколько решений к задачам SQL Academy
    Было прорешано более 50 задач в онлайн-тренажёре SQL Academy. 4 наиболее сложных и интересных решения с точки зрения кода запросов были добавлены сюда. Скриншот с подтверждением прохождения заданий есть в резюме (см. главный профиль).

---
+++ sql_academy_online/Task Solution #71 (MariaDB).sql
@@ -0,0 +1,22 @@
+/* Решение для задачи #71 тренажёра SQL Academy. */
+
+WITH Once_Count AS (
+	SELECT COUNT(*) AS count
+	FROM (
+		SELECT DISTINCT Users.id
+		FROM Users
+			INNER JOIN Reservations ON Users.id = Reservations.user_id
+		UNION
+		SELECT DISTINCT Users.id
+		FROM Users
+			INNER JOIN Rooms ON Users.id = Rooms.owner_id
+			INNER JOIN Reservations ON Rooms.id = Reservations.room_id
+	) AS A
+)
+
+SELECT
+ROUND((
+    SELECT *
+	FROM Once_Count
+) / COUNT(*) * 100, 2) as percent
+FROM Users
